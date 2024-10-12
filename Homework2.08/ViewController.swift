33d4dde1cd91e175ca605dfb883e830f8d21170d
Ivanmamaev1 <mamaevivan92@gmail.com>
2024-10-12 13:23:32 +0300

    Initial Commit

---
+++ Homework2.08/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework2.08
+//
+//  Created by Ivan on 12.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
